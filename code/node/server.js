8904af98ec47f8247300112c43585f2a6a7d0092
franznkemaka <franz@rigle.co>
2024-02-22 16:16:53 +0100

    ✨ server

--- code/node/server.js
+++ code/node/server.js
@@ -10,6 +10,12 @@ app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
+app.get("/api/create-user", (req, res) => {
+  users.push({ id: users.length + 1, name: req.query.name });
+
+  res.send("ok");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
