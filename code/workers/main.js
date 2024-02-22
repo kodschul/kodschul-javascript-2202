3bf0861c5be201c55304f7fd208463001ce98e5a
franznkemaka <franz@rigle.co>
2024-02-22 09:44:02 +0100

    ✨ workers

--- code/workers/main.js
+++ code/workers/main.js
@@ -6,6 +6,10 @@ worker.onmessage = function (event) {
   console.log(`Worker said: ${JSON.stringify(event.data)}`);
 };
 
+worker.onerror = function () {
+  console.log("Worker had an error");
+};
+
 document.getElementById("rootButton").onclick = () => {
   worker.postMessage({
     name: "say hello",
--- code/workers/worker.js
+++ code/workers/worker.js
@@ -3,7 +3,14 @@ self.onmessage = async function (event) {
 
   if (message.name == "say hello") {
     // do something
+    await doWork();
     message.output = "I finished work!";
     self.postMessage(message);
   }
 };
+
+async function doWork() {
+  await new Promise((r) => setTimeout(r, 2000));
+
+  return true;
+}
