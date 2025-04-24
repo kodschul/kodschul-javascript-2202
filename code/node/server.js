44e8a8649a7940f1cf4f2fec3f5a6dd571cb06f4
franznkemaka <info@franznkemaka.com>
2025-04-24 12:20:46 +0200

    🚀

--- code/node/server.js
+++ code/node/server.js
@@ -1,8 +1,68 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
+app.use(bodyParser.json());
+
+/** FRONTEND */
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/home.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/client/login.html");
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname + "/client/signup.html");
+});
+
+/** BACKEND */
+
+const users = [
+  {
+    firstName: "Max",
+    lastName: "Mustermann",
+    email: "max@example.com",
+    password: "password3",
+  },
+];
+
+app.get("/api/users", (req, res) => {
+  const allUsers = users.map((user) => {
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    };
+  });
+
+  res.json(allUsers);
+});
+
+app.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.json(newUser);
+});
+
+app.post("/api/login", (req, res) => {
+  const loginData = req.body;
+  const user = users.find((u) => u.email === loginData.email);
+
+  if (!user) {
+    return res.status(401).json({ message: "User not found" });
+  }
+
+  if (user.password !== loginData.password) {
+    return res.status(401).json({ message: "Incorrect password" });
+  }
+
+  res.json({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  });
 });
 
 app.listen(4000, () => {
